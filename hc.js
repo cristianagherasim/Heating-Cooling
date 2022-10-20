1b914cbfc7305a7fbc3a4143072c0ce78680e245
Cristiana <cristiana.gherasim@gmail.com>
2022-10-20 08:28:05 -0400

    HeatingCooling

---
+++ hc.js
@@ -0,0 +1,11 @@
+let actualTemp = "82";
+let desiredTemp = "72";
+
+if (actualTemp === desiredTemp)
+{console.log (`Standby`)};
+
+if (actualTemp > desiredTemp)
+{console.log (`Run A/C`)};
+
+if (actualTemp < desiredTemp)
+{console.log (`Run Heat`)};
